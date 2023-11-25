3fa8158e313f29d62da9bd1379eaef8bc6dae213
tripti shergill <triptishergill@gmail.com>
2023-11-25 18:03:00 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+def get_todos(filepath='todo.txt'):
+    """Read a text file and return the list of
+    todo items
+     """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_local,filepath='todo.txt'):
+    """Write todo items list in text file"""
+
+    with open(filepath, 'w') as file:
+        file.writelines(todos_local)
+
+
+if __name__ == "__main__":
+    print(get_todos())
