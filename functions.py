da9180f013de6424068ad41585d25a7ef6db8a2e
tripti shergill <triptishergill@gmail.com>
2023-11-25 19:47:45 -0500

    Add constant and nit

--- functions.py
+++ functions.py
@@ -1,14 +1,17 @@
-def get_todos(filepath='todo.txt'):
+FILEPATH = "todo.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """Read a text file and return the list of
-    todo items
+     to-do items.
      """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_local,filepath='todo.txt'):
-    """Write todo items list in text file"""
+def write_todos(todos_local,filepath=FILEPATH):
+    """Write to-do items list in text file"""
 
     with open(filepath, 'w') as file:
         file.writelines(todos_local)
